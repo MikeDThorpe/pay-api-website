cb5431c542b9b1e8937e1783f363a0779e2e3f47
TTBoiMike <michaelthorpe747@hotmail.com>
2021-02-13 15:37:03 +0000

    init repo with homepage/pricing page complete

---
+++ script.js
@@ -0,0 +1,10 @@
+let hamburger = document.querySelector(".hamburger");
+let close = document.getElementById("nav-close");
+
+function openNavBar() {
+    document.querySelector(".mobile-nav").style.display = "block";
+}
+function closeNavBar() {
+    document.querySelector(".mobile-nav").style.display = "none";
+
+}
