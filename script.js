dae4a11444ba2d1739affabbb2f03f390e9d527b
TTBoiMike <michaelthorpe747@hotmail.com>
2021-02-20 10:50:14 +0000

    add from validation

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function closeNavBar() {
     document.querySelector(".mobile-nav").style.display = "none";
 
 }
+
+// contact form submission / vaidation
+let contactForm = document.forms["contact-form"]
+let formInputs = contactForm.getElementsByTagName("input")
+contactForm.addEventListener("submit", (e) => {
+    for(let i = 0; i < formInputs.length; i++) {
+        if(formInputs[i].value === "" || formInputs[i].value === null) {
+            e.preventDefault()
+            formInputs[i].style.borderBottom = "2px solid #FF0000"
+            formInputs[i].classList.add("ph-error")
+            formInputs[i].nextElementSibling.innerHTML = "<small>Required</small>"
+        } else if (formInputs[i].style.borderBottom = "2px solid #FF0000") {
+            // remove styling if input is completed
+            formInputs[i].style.borderBottom = "2px solid var(--san-juan-blue)"
+            formInputs[i].nextElementSibling.innerHTML = ""
+        }
+    }
+})
